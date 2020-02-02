106f8c93eaaf29134eb6863a9e6322534a5d4f74
adriancoliba <adrian.coliba5@gmail.com>
2020-02-02 22:33:25 +0000

    Added PostgreSQL settings and necessary dependencies.

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ dotenv.config();
 const express = require('express');
 const app = new express();
 
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const JwtStrategy = passportJWT.Strategy;
+const ExtractJwt = passportJWT.ExtractJwt;
+
+const opts = { 
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), 
+    secretOrKey: process.env.SECRET_OR_KEY
+}
+const strategy = new JwtStrategy(opts, (payload, next) => {
+    const user = null;
+    next(null, user)
+})
+passport.use(strategy)
+app.use(passport.initialize())
+
 app.get('/', (req, res) => {
     res.send('Hey Hello');
 })
